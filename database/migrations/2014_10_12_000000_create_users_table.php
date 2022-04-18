c784725f22feb9c88277bd226e4604b50411ee37
Timotej Avsec <avsectimotej1@gmail.com>
2022-04-18 19:45:57 +0200

    Add Gitlab ID to user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->longText("avatar")->nullable();
+            $table->string("gitlab_id")->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
